0a0a399521f42a5896540c67074f39d951032f09
Alexandre Muniz <aom@puzzlezapper.com>
2020-02-14 14:10:42 -0800

    Removed LOADING actions, we going to go a different way.

--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -1,8 +1,6 @@
 import { 
   FETCH_USER,
-  FETCH_USER_LOADING,
   FETCH_REPOS,
-  FETCH_REPOS_LOADING
 } from '../constants/action-types';
 
 export const fetchUser = (user) => ({
@@ -10,17 +8,7 @@ export const fetchUser = (user) => ({
   payload: user
 });
 
-export const fetchUserLoading = (user) => ({
-  type: FETCH_USER_LOADING,
-  payload: user
-});
-
 export const fetchRepos = (user) => ({
   type: FETCH_REPOS,
-  payload: user
-});
-
-export const fetchReposLoading = (user) => ({
-  type: FETCH_REPOS_LOADING,
   payload: user
 });
