f5e8c4e3883c04ff5777883c622f1acd823555ed
Alexandre Muniz <aom@puzzlezapper.com>
2020-02-14 15:49:58 -0800

    cors-anywhere to bypass rate-limiting

--- src/services/github-api.js
+++ src/services/github-api.js
@@ -1,5 +1,5 @@
 const request = path => {
-  return fetch(`https://api.github.com/${path}`)
+  return fetch(`https://cors-anywhere.herokuapp.com/api.github.com/${path}`)
     .then(res => Promise.all([res.ok, res.json()]))
     .then(([ok, json]) => {
       if(!ok) throw json;
