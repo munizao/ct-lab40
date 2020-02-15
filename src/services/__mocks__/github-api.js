5cca0d58da6365867e673387fa6bbf92bd555473
Alexandre Muniz <aom@puzzlezapper.com>
2020-02-14 17:39:47 -0800

    moved request function and mock

--- src/services/__mocks__/github-api.js
+++
@@ -1,1 +0,0 @@
-export const request = () => Promise.resolve(['user data']);
--- src/services/github-api.js
+++ src/services/github-api.js
@@ -1,12 +1,4 @@
-const request = path => {
-  return fetch(`https://cors-anywhere.herokuapp.com/api.github.com/${path}`)
-    .then(res => Promise.all([res.ok, res.json()]))
-    .then(([ok, json]) => {
-      if(!ok) throw json;
-
-      return json;
-    });
-};
+import request from './request';
 
 export const getUser = (user) => request(`/users/${user}`);
 export const getRepos = (user) => request(`/users/${user}/repos`);
