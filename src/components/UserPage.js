971b4d20ec941e62cd76a23d44b2b22fc7d189ef
Alexandre Muniz <aom@puzzlezapper.com>
2020-02-14 16:32:29 -0800

    made UserDisplay component

--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -1,13 +1,12 @@
 import React, { useState } from 'react';
 import { fetchUser } from '../redux/actions/actions';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectUser, selectLoading } from '../redux/selectors/selector';
+import { useDispatch } from 'react-redux';
+import UserDisplay from './UserDisplay';
 
 const UserPage = () => {
   const [userName, setUserName] = useState('');
   const dispatch = useDispatch();
-  const userInfo = useSelector(selectUser);
-  const loading = useSelector(selectLoading);
+
   return (
     <>
       <header>
@@ -18,9 +17,7 @@ const UserPage = () => {
           <input type="text" value={userName}  onChange={({ target }) => setUserName(target.value)} />
           <button onClick={() => dispatch(fetchUser(userName))}>Submit</button>
         </div>
-        <div>
-          {loading ? 'Loading...' : JSON.stringify(userInfo)}
-        </div>
+        <UserDisplay />
       </main>
     </>
   );
