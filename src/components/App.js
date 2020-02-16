fd30793f75e9d1cc2c52ea091072565712f7f6bd
Alexandre Muniz <aom@puzzlezapper.com>
2020-02-15 21:57:50 -0800

    Removed commented out stuff

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,4 @@
 import React from 'react';
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route
-// } from 'react-router-dom';
 import UserPage from './UserPage';
 
 export default function App() {
